5e9332a28dc7d78ca25f1d18c869f5ec64dac108
michlocurcio <michlocurcio@gmail.com>
2021-08-07 14:12:51 +0200

    Terminamos

---
+++ salarios.js
@@ -0,0 +1,49 @@
+const colombia = [];
+colombia.push({
+    name: "Jess",
+    salary: 1500,
+});
+colombia.push({
+    name: "Rigatoni",
+    salary: 2500,
+});
+colombia.push({
+    name: "Oreo",
+    salary: 2000,
+});
+colombia.push({
+    name: "Luis",
+    salary: 1000,
+});
+colombia.push({
+    name: "Daniel",
+    salary: 1000,
+});
+colombia.push({
+    name: "Caro",
+    salary: 800,
+});
+colombia.push({
+    name: "Nala",
+    salary: 1600,
+});
+colombia.push({
+    name: "inky",
+    salary: 1800,
+});
+colombia.push({
+    name: "Kira",
+    salary: 1600,
+});
+colombia.push({
+    name: "Lea",
+    salary: 600,
+});
+colombia.push({
+    name: "Linda",
+    salary: 1200,
+});
+colombia.push({
+    name: "Fonsi",
+    salary: 1300,
+});
